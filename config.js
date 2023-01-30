ec494f99fb0077a1dc331dfba7ec76f89e4f2d3a
Jacques von Hämsterviel <59958130+JacquesVonHamsterviel@users.noreply.github.com>
2023-01-30 00:36:16 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ var blog_name="Sheng's Blog";
 
 //插件
 
-//板娘插件
+//板娘插件（https://github.com/fghrsh/live2d_demo）
 const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/"; // 注意：live2d_path 参数应使用绝对路径
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
@@ -34,5 +34,6 @@ Promise.all([
 	loadExternalResource(live2d_path + "live2d.min.js", "js"),
 	loadExternalResource(live2d_path + "waifu-tips.js", "js")
 ]).then(() => {
+	live2d_settings['waifuEdgeSide']='right:30';
 	initWidget(live2d_path + "waifu-tips.json", "https://live2d.fghrsh.net/api");
 });
