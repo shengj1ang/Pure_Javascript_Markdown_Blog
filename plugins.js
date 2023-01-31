c8a2fd74d98163031682c4af2ac7ca88dee8b160
Jacques von Hämsterviel <59958130+JacquesVonHamsterviel@users.noreply.github.com>
2023-01-31 21:36:57 +0000

    20230123b
    20230123b

--- plugins.js
+++ plugins.js
@@ -5,46 +5,11 @@
 
 以下部分为插件，不需要的可以删掉或者注释掉
 
-目前有的功能：板娘插件，点击页面的烟花效果
+目前有的功能：点击页面的烟花效果，板娘插件
 
 */
 
 //----------------------------------------------------分割线----------------------------------------------------------------------------------------
-
-//板娘插件（https://github.com/fghrsh/live2d_demo）
-const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/"; // 注意：live2d_path 参数应使用绝对路径
-// 封装异步加载资源的方法
-function loadExternalResource(url, type) {
-	return new Promise((resolve, reject) => {
-		let tag;
-
-		if (type === "css") {
-			tag = document.createElement("link");
-			tag.rel = "stylesheet";
-			tag.href = url;
-		}
-		else if (type === "js") {
-			tag = document.createElement("script");
-			tag.src = url;
-		}
-		if (tag) {
-			tag.onload = () => resolve(url);
-			tag.onerror = () => reject(url);
-			document.head.appendChild(tag);
-		}
-	});
-}
-// 加载 waifu.css live2d.min.js waifu-tips.js
-Promise.all([
-	loadExternalResource("/plugins/live2d-widget/waifu_right.css", "css"),
-	loadExternalResource(live2d_path + "live2d.min.js", "js"),
-	loadExternalResource(live2d_path + "waifu-tips.js", "js")
-]).then(() => {
-	initWidget(live2d_path + "waifu-tips.json", "https://live2d.fghrsh.net/api");
-});
-
-//----------------------------------------------------分割线----------------------------------------------------------------------------------------
-
 //烟花效果
 //粒子颜色,根据爱好来选择
 const Colors = [
@@ -104,6 +69,41 @@ document.addEventListener("click",(e) =>{
    }
 
 });
+
+//----------------------------------------------------分割线----------------------------------------------------------------------------------------
+
+//板娘插件（https://github.com/fghrsh/live2d_demo）
+const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/"; // 注意：live2d_path 参数应使用绝对路径
+// 封装异步加载资源的方法
+function loadExternalResource(url, type) {
+	return new Promise((resolve, reject) => {
+		let tag;
+
+		if (type === "css") {
+			tag = document.createElement("link");
+			tag.rel = "stylesheet";
+			tag.href = url;
+		}
+		else if (type === "js") {
+			tag = document.createElement("script");
+			tag.src = url;
+		}
+		if (tag) {
+			tag.onload = () => resolve(url);
+			tag.onerror = () => reject(url);
+			document.head.appendChild(tag);
+		}
+	});
+}
+// 加载 waifu.css live2d.min.js waifu-tips.js
+Promise.all([
+	loadExternalResource("/plugins/live2d-widget/waifu_right.css", "css"),
+	loadExternalResource(live2d_path + "live2d.min.js", "js"),
+	loadExternalResource(live2d_path + "waifu-tips.js", "js")
+]).then(() => {
+	initWidget(live2d_path + "waifu-tips.json", "https://live2d.fghrsh.net/api");
+});
+
 
 
 //----------------------------------------------------分割线----------------------------------------------------------------------------------------
