2dc9d286ba2bb0e08276bb08a156846d37865df5
Jacques von Hämsterviel <59958130+JacquesVonHamsterviel@users.noreply.github.com>
2023-04-23 16:09:35 +0100

    20230423
    20230423

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
 var blog_name="Sheng's Blog";
+var allowedDomains = ["https://sheng.pages.dev/", "https://blog.example.com/", "https://sheng.kkk.plus/"];
+var PathRewriteDomains = ["sheng.pages.dev"]
