1920660333340914fc2d74a1f4fd7d8e474e325e
Jacques von Hämsterviel <59958130+JacquesVonHamsterviel@users.noreply.github.com>
2023-05-01 22:32:50 +0100

    startYear in copyright
    startYear in copyright

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 var blog_name="Sheng's Blog";
+var startYear = "2018";
 var allowedDomains = ["localhost","127.0.0.1","sheng.pages.dev", "blog.example.com", "sheng.kkk.plus"];
-var PathRewriteDomains = ["localhost","127.0.0.1","sheng.pages.dev"]
-var ForceJumpBack = "https://sheng.pages.dev/"
+var PathRewriteDomains = ["localhost","127.0.0.1","sheng.pages.dev"];
+var ForceJumpBack = "https://sheng.pages.dev/";
